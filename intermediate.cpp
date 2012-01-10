526071d1963ae78c314be85928dca69bfcc8472f
Marek Brzóska <brzoskamarek@gmail.com>
2012-01-10 08:45:18 +0100

    exc 7.4

--- intermediate.cpp
+++ intermediate.cpp
@@ -1,4 +1,3 @@
-
 #include "error.h"
 #include "identifier.h"
 #include "primtype.h"
@@ -11,13 +10,142 @@
 #include "substitution.h"
 #include "varstore.h"
 #include "typesystem.h"
+#include <set>
+#include <list>
+#include <vector>
+#include <iostream>
+#include <typeinfo>
 
 #include "identifier.stat"
 
 
+using namespace std;
+
+
+// Prints list of trees. This may be useful:
+ostream& operator<< ( ostream& stream, const list<tree> & treelist )
+{
+   for( list<tree>::const_iterator p = treelist. begin(); p != treelist. end( ); ++p )
+   {
+      stream << "   " << *p << "\n";
+   }
+   stream << "\n";
+   return stream;
+}
+
+
+list< tree > functionlist()
+{
+    // Marek: exrc 2
+    list< tree > flist;
+    list< treetype > argtypes;
+
+
+    vector< char > attr;// = vector< char > :: vector( sizeof( int ), 0 );
+
+
+    treetype tt_int = treetype( type_prim, primtype( "int", sizeof(int)));
+    argtypes. push_back( tt_int );
+    argtypes. push_back( tt_int );
+    attr. clear( );
+    flist. push_back( tree::tree( "+", attr, treetype::treetype( type_func, tt_int, argtypes )));
+
+
+    treetype tt_double = treetype( type_prim, primtype( "double", sizeof( double )));
+    argtypes. clear( );
+    argtypes. push_back( tt_double );
+    argtypes. push_back( tt_double );
+    flist. push_back( tree::tree( "+", attr, treetype::treetype( type_func, tt_double, argtypes )));
+
+
+    ////treetype tt_char = treetype( type_prim, primtype( "char", sizeof(char)));
+      //ts. convfunctions. push_back( typesystem::convfunc( "conv_char_int", 5,
+      //                                        tt_char, tt_int )); 
+      //ts. convfunctions. push_back( typesystem::convfunc( "conv_char_double", 
+      //                                        50,
+      //                                        tt_char, tt_double ));
+
+    return flist;
+}
+
+list< tree > fewconversions()
+{
+    // Marek: exrc 3
+    list< tree > clist;
+    list< treetype > argtypes;
+
+
+    vector< char > attr;
+
+
+    treetype tt_int = treetype( type_prim, primtype( "int", sizeof(int)));
+    treetype tt_bool = treetype( type_prim, primtype( "bool", sizeof(bool)));
+    treetype tt_double = treetype( type_prim, primtype( "double", sizeof( double )));
+
+    argtypes. push_back( tt_int );
+    attr. clear( );
+    clist. push_back( tree::tree( "conv_int_double", attr, treetype::treetype( type_func, tt_double, argtypes )));
+
+
+    argtypes. clear( );
+    argtypes. push_back( tt_double );
+    clist. push_back( tree::tree( "conv_double_int", attr, treetype::treetype( type_func, tt_int, argtypes )));
+
+    argtypes. clear( );
+    argtypes. push_back( tt_bool );
+    clist. push_back( tree::tree( "conv_bool_int", attr, treetype::treetype( type_func, tt_int, argtypes )));
+
+
+    ////treetype tt_char = treetype( type_prim, primtype( "char", sizeof(char)));
+
+    return clist;
+}
+
+list<tree> possibleconversions(const list< tree > & conversionlist, tree t) {
+   // Marek: exrc 4
+   bool needtocontinue;
+
+   set<tree> possibleconvs;
+   set<treetype> possibletypes;
+   pair<set<treetype>::iterator, bool> insertresult;
+
+   list<tree>::const_iterator conversion;
+   possibletypes.insert(t.tt);
+   do {
+      needtocontinue = false;
+      for (conversion = conversionlist.begin(); conversion != conversionlist.end(); ++conversion) {
+         if ( possibletypes.count(conversion->tt.getargtype(0)) ) {
+            needtocontinue |= possibletypes.insert(conversion->tt.getresulttype()).second;
+            possibleconvs.insert(*conversion);
+         }
+      }
+   } while ( needtocontinue );
+
+   list<tree> result;
+   for (set<tree>::iterator conv = possibleconvs.begin(); conv != possibleconvs.end(); ++conv) {
+      result.push_back(*conv);
+   }
+
+   return result;
+}
+
+
+
+
 int main( int argc, char* argv [ ] )
 {
-   std::cout << "hallo\n";
+   treetype tt_int = treetype( type_prim, primtype( "int", sizeof(int)));
+   treetype tt_bool = treetype( type_prim, primtype( "bool", sizeof(bool)));
+   tree zero = tree( "zero", tobinary(0), tt_int );
+   tree fls = tree( "false", tobinary(false), tt_bool );
+   cout << 
+       "fewconversions:\n" << fewconversions() << endl << 
+       "zero:\n" << zero << endl << endl;
+   cout << "possible conversions for zero:" << endl;
+   cout << possibleconversions(fewconversions(), zero) << "\n";
+   cout << "possible conversions for false:" << endl;
+   cout << possibleconversions(fewconversions(), fls) << "\n";
+   return 0;
 
    for( unsigned int i = 0; i < 1; ++ i )
    {
@@ -27,8 +155,8 @@ int main( int argc, char* argv [ ] )
       treetype tt1 = treetype( type_prim, p1 );
       treetype tt2 = treetype( type_prim, p2 );
 
-      std::list< treetype > args1;
-      std::list< treetype > args2;
+      list< treetype > args1;
+      list< treetype > args2;
 
       args1. push_back( treetype( type_prim, p1 ));
       args1. push_back( treetype( type_void ));
@@ -37,11 +165,11 @@ int main( int argc, char* argv [ ] )
       args2. push_back( treetype( type_prim, p1 ));
       args2. push_back( treetype( type_void ));
 
-      std::cout << ( treetype( type_func, tt1, args1 ) == 
+      cout << ( treetype( type_func, tt1, args1 ) == 
                      treetype( type_func, tt1, args1 ) ) << "\n";
  
       treetype g = treetype( type_func, tt2, args1 );
-      std::cout << g. getargtype(2) << "\n";
+      cout << g. getargtype(2) << "\n";
 
 
       // Here starts code that you need in exercise 9. 
@@ -58,7 +186,7 @@ int main( int argc, char* argv [ ] )
                       tobinary( 3.141592653589793238462643383279 ),
                       tt_double );
 
-      pi. prettyprint( std::cout );  
+      pi. prettyprint( cout );  
       
       tree e = tree( "e", 
                      tobinary( 2.718281828459045235360287471352 ),
@@ -68,14 +196,14 @@ int main( int argc, char* argv [ ] )
       tree one = tree( "one", tobinary(1), tt_int );
       tree two = tree( "two", tobinary(2), tt_int );
       tree t = tree( "+", tree( "*", pi, zero ), two ); 
-      t. prettyprint( std::cout );
+      t. prettyprint( cout );
 
       // We put a few function declarations in functionlist.
       // Can be used as example in task 2 and task 3: 
 
       typesystem ts; 
      
-      std::list< treetype > argtypes;
+      list< treetype > argtypes;
       argtypes. push_back( tt_int );
       argtypes. push_back( tt_int );
  
@@ -103,7 +231,7 @@ int main( int argc, char* argv [ ] )
                               treetype( type_prim, primtype( "X", 0 ))),
                               argtypes )));
               
-      std::cout << "Typesystem:\n\n";
+      cout << "Typesystem:\n\n";
 
       ts. convfunctions. push_back( typesystem::convfunc( "conv_int_double", 4,
                                               tt_int, tt_double )); 
@@ -121,9 +249,9 @@ int main( int argc, char* argv [ ] )
                                               treetype( type_pointer, 
                                                  treetype( type_prim,
                                                     primtype( "X", 0 )))));
-      std::cout << ts << "\n";
+      cout << ts << "\n";
 
-      std::cout << ts. allconversions( 
+      cout << ts. allconversions( 
                   tree( "x", tobinary(44), 
                     treetype( type_ref, 
                        treetype( type_array, tt_char ))));
@@ -180,7 +308,7 @@ int main( int argc, char* argv [ ] )
       program pr;
       pr. insert( "main", treetype( type_void ), fl ); 
       pr. insert( "hallo", treetype( type_void ), fl ); 
-      std::cout << pr << "\n";
+      cout << pr << "\n";
 
       runtimestate rs( 256 );
       rs. tracing = true;
@@ -191,12 +319,15 @@ int main( int argc, char* argv [ ] )
       }
       catch( const error& err )
       {
-         std::cout << err << "\n";
+         cout << err << "\n";
       }
      
    }
-   std::cout << "\n";
-   identifier::printnormtable( std::cout );
+   cout << "\n";
+   identifier::printnormtable( cout );
+
+
+
 
 }
 
